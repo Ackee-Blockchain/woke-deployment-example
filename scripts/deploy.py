85f27969ee521ac3d073d430cacb108e0dc498a0
Michal Převrátil <michprev@gmail.com>
2023-03-20 19:00:30 +0100

    ✨ Add example contract and deployment script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,5 @@
 from woke.deployment import *
+from pytypes.contracts.Counter import Counter
 
 NODE_URL = "ENTER_NODE_URL_HERE"
 
@@ -6,3 +7,6 @@ NODE_URL = "ENTER_NODE_URL_HERE"
 @default_chain.connect(NODE_URL)
 def main():
     default_chain.set_default_accounts(Account.from_alias("deployment"))
+
+    counter = Counter.deploy()
+    print("Counter deployed at", counter.address)
